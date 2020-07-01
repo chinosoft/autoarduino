bca328fa09d6a540dab4ba6f0a795287257a316b
chinosoft <chinosoft@gmail.com>
2020-07-01 19:38:26 -0300

    mm

--- src/app/services/sensor.service.ts
+++ src/app/services/sensor.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from "rxjs";
+import { map, catchError } from "rxjs/operators";
+import { ISensor } from './../interfaces/sensor';
+
+
+import { Board, Led } from 'johnny-five'; 
 
 @Injectable({
   providedIn: 'root'
 })
 export class SensorService {
 
-  constructor() { }
+  constructor(private sensor: ISensor) { }
+
+  public board = new Board().on('ready', () => { const led = new Led(13); led.blink(500); })
+ 
+   
+  
 }
